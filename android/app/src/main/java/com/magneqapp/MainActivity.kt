30aad29e8598720d87b431cc660a8a6478a3e907
pathaksoham2003 <pathaksoham2003@gmail.com>
2025-07-24 01:32:49 +0530

    Fixed the kotlin issue

--- android/app/src/main/java/com/magneqapp/MainActivity.kt
+++ android/app/src/main/java/com/magneqapp/MainActivity.kt
@@ -4,10 +4,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-<<<<<<< HEAD
 import android.os.Bundle;
-=======
->>>>>>> 366337f5fd26645a8daa3eff1d3c04f83682df08
 
 class MainActivity : ReactActivity() {
 
@@ -17,23 +14,15 @@ class MainActivity : ReactActivity() {
    */
   override fun getMainComponentName(): String = "MagneqApp"
 
-<<<<<<< HEAD
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
   }
-=======
->>>>>>> 366337f5fd26645a8daa3eff1d3c04f83682df08
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
-<<<<<<< HEAD
   
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 
-=======
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
->>>>>>> 366337f5fd26645a8daa3eff1d3c04f83682df08
 }
